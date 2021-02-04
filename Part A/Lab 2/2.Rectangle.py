a50b51ff32686c40e32f927079d4c2cc78b8e64e
Nitin C Kallappa <nitinckallappa@gmail.com>
2021-02-04 09:49:02 +0530

    Update 2.Rectangle.py

--- Part A/Lab 2/2.Rectangle.py
+++ Part A/Lab 2/2.Rectangle.py
@@ -1,3 +1,10 @@
+"""
+Write a python program to create a class ‘Rectangle’.
+This class should include a constructor to initialize the dimensions. 
+Include a function in the class to compute the area of the rectangle.
+Create objects of the class and print area.
+"""
+
 class Rectangle:
 	def __init__(self):
 		self.l = int(input("Enter length"))
