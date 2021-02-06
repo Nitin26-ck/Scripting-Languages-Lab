5eedd61fece4e11607658dba59995e585863edb1
Nitin C Kallappa <nitinckallappa@gmail.com>
2021-02-06 10:40:48 +0530

    Update 5.red.py

--- Part A/Lab 5/5.red.py
+++ Part A/Lab 5/5.red.py
@@ -1,3 +1,9 @@
+"""
+Create a list of 6 numbers.
+Use ‘list-comprehension’ to create a new list where each element in the original list is multiplied by 3.
+Use ‘lambda’ and ‘reduce’ function to find the sum of the elements of the original list as well as the new list.
+"""
+
 from functools import reduce
 mylist=[1,2,3,4,5,6]
 print("Original",mylist)
