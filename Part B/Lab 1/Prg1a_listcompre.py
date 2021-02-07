5ef26e98240cc38534662d0e929c48f220cf9b96
Nitin C Kallappa <nitinckallappa@gmail.com>
2021-02-07 10:15:01 +0530

    Update Prg1a_listcompre.py

--- Part B/Lab 1/Prg1a_listcompre.py
+++ Part B/Lab 1/Prg1a_listcompre.py
@@ -1,6 +1,10 @@
-#Write a python program to read in a list of numbers.
-#Use one-line comprehensions of create a new list of even numbers.
-#Create another list reversing the elements.
+"""Introduction to Python:
+​Write Python programs to do the following: 
+a) Read a list of elements. Create a new list having all the elements minus the duplicates (Use functions).
+Use one-line comprehensions to create a new list of even numbers.
+Create another list reversing the elements.
+"""
+
 S = [x**2 for x in range(10)] # read elements  to list
 M = [x for x in S if x % 2 == 0]
 M.reverse()
