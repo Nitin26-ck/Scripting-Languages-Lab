ce14de28ee0c36e47f987f215d9008ed4959119f
Nitin C Kallappa <nitinckallappa@gmail.com>
2021-02-09 23:47:50 +0530

    Update 2.py

--- Part B/Lab 2/2.py
+++ Part B/Lab 2/2.py
@@ -1,3 +1,12 @@
+"""
+Introduction to Python Functions:
+Write a temperature converter python program, which is menu driven.
+Each such conversion logic should be defined in separate functions.
+The program should call the respective function based on the user’s requirement.
+The program should run as long as the user wishes so. 
+Provide an option to view the conversions stored as a list of tuples with attributes - from unit value, to unit value sorted by the user’s choice (from-value or to-value). 
+"""
+
 def celciustofarenhiet(c):
     return (c*(9/5) + 32)
 
